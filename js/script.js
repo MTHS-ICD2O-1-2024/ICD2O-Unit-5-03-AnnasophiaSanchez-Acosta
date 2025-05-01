99bf5855a41c7aa52e9cbaf4099d770b51e1be08
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-01 16:46:41 +0000

    js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@
 
 "use strict"
 
+/**
+ * This function checks users age 
+ */
+
+
+function checkAge() {
+  // input
+  const userAge = document.getElementById("user-age").checked
+
+  // process
+  if (userAge >= 17) {
+    //output
+    document.getElementById("result").innerHTML =
+      "You can watch R rated movies alone"
+      //output
+  } else if (userAge >= 13) {
+    document.getElementById("result").innerHTML =
+      "You can watch PG-13 movies alone"
+      //output
+  } else if (userAge >= 5) {
+    document.getElementById("result").innerHTML =
+      "You can watch  g or pg movies alone"
+      //output
+  } else {
+    document.getElementById("result").innerHTML =
+      "You are to young for most movies"
+  }
+}
